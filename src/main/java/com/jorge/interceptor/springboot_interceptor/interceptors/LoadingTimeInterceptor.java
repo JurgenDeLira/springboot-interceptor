8ad083b7f8d4f3209635a9bc130d4cf4c2cab55a
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-12-18 19:55:45 -0700

    Implemente interceptor TiempoTranscurrido

--- src/main/java/com/jorge/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/jorge/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -10,22 +10,37 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Random;
+
 @Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                           @Nullable ModelAndView modelAndView) throws Exception {
-        logger.info("LoadingTimeInterceptor: postHandle() saliendo ...." + ((HandlerMethod)handler).getMethod().getName());
-
-    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HandlerMethod controller = ((HandlerMethod)handler);
         logger.info("LoadingTimeInterceptor: preHandle() entrando ...." + controller.getMethod().getName());
+
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           @Nullable ModelAndView modelAndView) throws Exception {
+
+        long end = System.currentTimeMillis();
+        long start = (long) request.getAttribute("start");
+        long result = end - start;
+        logger.info("Tiempo transcurrido: " + result + " milisegundos!");
+        logger.info("LoadingTimeInterceptor: postHandle() saliendo ...." + ((HandlerMethod)handler).getMethod().getName());
+
     }
 }
